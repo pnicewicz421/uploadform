36eda856bb54a88916d15659ab77a02693afeeaa
Peter Nicewicz <peternicewicz@Peters-MacBook-Pro.local>
2015-11-21 15:54:09 -0500

    Index_View returns correct title

--- rhymis/tests.py
+++ rhymis/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from .views import index_view
 
 # Create your tests here.
@@ -8,3 +10,11 @@ class index_viewTest(TestCase):
     def test_root_url_resolves_to_index_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index_view)
+        
+    def test_index_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = index_view(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Issue Tracker</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+        
--- rhymis/views.py
+++ rhymis/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index_view():
-    pass
+def index_view(request):
+    myresponse = '<html><title>Issue Tracker</title></html>'
+    return HttpResponse(myresponse)
