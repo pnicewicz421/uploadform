78d88a1abc1c0da858f6eafa06aa53d64fb551bb
Peter Nicewicz <peternicewicz@Peters-MacBook-Pro.local>
2016-01-25 08:18:15 -0500

    Version with calendar feature and blocked html.

---
+++ rhymis/static/app.js
@@ -0,0 +1,11 @@
+/*
+I am playing around with JavaScript and am determined to incorporate it into my webpage
+*/
+var a = 'Variable';
+
+b();
+
+function b() {
+    console.log('Called b!');
+    }
+    
